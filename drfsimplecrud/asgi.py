824f5b8cfa809f9304a6f531f3c9b7c92daf3df9
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2022-10-16 22:58:24 -0500

    Update asgi.py

--- drfsimplecrud/asgi.py
+++ drfsimplecrud/asgi.py
@@ -1,8 +1,6 @@
 """
 ASGI config for drfsimplecrud project.
-
 It exposes the ASGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
